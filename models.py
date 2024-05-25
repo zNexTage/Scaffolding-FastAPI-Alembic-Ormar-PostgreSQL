2b24af0efbd8465da456ebe955ec9039c7cb7410
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-05-25 12:16:18 -0300

    Criado modelo: Shelter

--- models.py
+++ models.py
@@ -5,3 +5,14 @@ import ormar
 # class Usuario(ormar.Model):
 #     class Meta(BaseMeta):
 #         tablename="tbl_usuario" 
+
+class Shelter(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "Abrigos"
+
+    id:str = ormar.UUID(uuid_format= 'hex', primary_key=True)
+    name:str = ormar.String(max_length=200)
+    address:str = ormar.String(max_length=200)
+    whats_app = ormar.String(max_length=200)
+    lat = ormar.Decimal(precision=19, scale=0)
+    lng = ormar.Decimal(precision=19, scale=0)
